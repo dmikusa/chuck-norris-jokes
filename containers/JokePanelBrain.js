adb6b58bbc72d3ced980a8709173a9d82e4bd212
Daniel Mikusa <dmikusa@pivotal.io>
2016-08-24 07:38:13 -0400

    Added container to hold data.  Not totally necessary in this very small example, but good form.

---
+++ containers/JokePanelBrain.js
@@ -0,0 +1,25 @@
+var types = require('../constants/ActionTypes');
+var actions = require('../actions');
+var connect = require('react-redux/lib/components/connect').default;
+var JokePanel = require('../components/JokePanel');
+
+var mapStateToProps = function(state) {
+    return {
+        title: state.joke.title,
+        joke: state.joke.body
+    };
+};
+
+var mapDispatchToProps = function(dispatch) {
+    return {
+        refresh: function(e) {
+            console.log('refresh clicked');
+            dispatch(actions.refresh());
+        }
+    };
+};
+
+module.exports = connect(
+                     mapStateToProps,
+                     mapDispatchToProps
+                 )(JokePanel);
