3c75026dfdcdfff530681a13520d029a74355c75
Daniel Mikusa <dmikusa@pivotal.io>
2016-08-31 23:12:30 -0400

    Fixed typo

--- store/index.js
+++ store/index.js
@@ -1,6 +1,6 @@
 var thunkMiddleware = require('redux-thunk').default;
 var createStore = require('redux/lib/createStore').default;
-var createStore = require('redux/lib/applyMiddleware').default;
+var applyMiddleware = require('redux/lib/applyMiddleware').default;
 var rootReducer = require('../reducers/root');
 
 var store = createStore(
